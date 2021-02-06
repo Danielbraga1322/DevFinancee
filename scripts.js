77f51c48451449368bf8517d1a7da9262ae8d5e0
Danielbraga1322 <dcb.1305@gmail.com>
2021-02-05 23:24:06 -0300

    cont. aula3
    Adiconado mais algumas funcionalidades no java script

--- scripts.js
+++ scripts.js
@@ -20,41 +20,70 @@ const Modal = {
 
 const transactions = [
 {
-        id: 1,
         description: 'Luz',
         amount: -50000,
         date: '23/01/2021',
 },
 {
-        id: 2,
         description: 'Website',
-        amount: 5000000,
+        amount: 500000,
         date: '23/01/2021',
 },
 {
-        id: 3,
         description: 'Internet',
         amount: -20000,
+        date: '23/01/2021',
+},
+{
+        description: 'app',
+        amount: 80000,
         date: '23/01/2021',
 }
 ]
 
 const Transaction = {
-    incomes () {
+    all:transactions,
 
+    add(transaction){
+        Transaction.all.push(transaction)
+
+        App.reload()
+    },
+
+    remove(index){
+        Transaction.all.splice(index,1)
+
+        App.reload()
+    },
+
+    incomes () {
+        let income = 0;
+        Transaction.all.forEach(transaction => {
+            if(transaction.amount>0) {
+                income = income + transaction.amount;
+            }
+        })
+      
+        return income
     },
     expenses () {
-
+        let expense = 0;
+        Transaction.all.forEach(transaction => {
+            if(transaction.amount<0) {
+                expense = expense + transaction.amount;
+            }
+        })
+        return expense
     },
     total () {
-
+        return Transaction.incomes() + Transaction.expenses()
     }
 }
 
 const DOM = {
     transactionsContainer: document.querySelector('#data-table tbody'),
 
-    addTransaction(transaction, index){
+    addTransaction(transaction, index) {
         const tr = document.createElement('tr')
         tr.innerHTML = DOM.innerHTMLTransaction(transaction)
 
@@ -64,20 +93,73 @@ const DOM = {
     innerHTMLTransaction (transaction) {
         const CSSclass = transaction.amount > 0 ? "income": "expense"
 
+        const amount = Utils.formatCurrancy(transaction.amount)
+
         const html = `
             <td class="description">${transaction.description}</td>
-            <td class="${CSSclass}">${transaction.amount}</td>
+            <td class="${CSSclass}">${amount}</td>
             <td class="date">${transaction.date}</td>
             <td>
             <img src="./assets/minus.svg" alt="Remover transação"/>
             </td>
         `
         return html
+    },
+
+    updateBalance() {
+        document
+            .getElementById("incomeDisplay")
+            .innerHTML = Utils.formatCurrancy(Transaction.incomes())
+        document
+            .getElementById("expenseDisplay")
+            .innerHTML = Utils.formatCurrancy(Transaction.expenses())
+        document
+            .getElementById("totalDisplay")
+            .innerHTML = Utils.formatCurrancy(Transaction.total())
+    },
+
+    clearTransactions() {
+        DOM.transactionsContainer.innerHTML = ""
     }
 }
 
-transactions.forEach(function(transaction){
-    DOM.addTransaction(transaction)
+const Utils = {
+    formatCurrancy(value){
+        const signal = Number(value) < 0 ? "-" : ""
+
+        value = String(value).replace(/\D/g, "")
+
+        value = Number(value) / 100
+
+        value = value.toLocaleString("pt-Br", {
+            style: "currency",
+            currency: "BRL"
+        })
+
+        return signal + value
+    }
+}
+
+const App = {
+    init() {
+        Transaction.all.forEach(transaction => {
+            DOM.addTransaction(transaction)
+        })
+        
+        DOM.updateBalance()
+    },
+    reload() {
+        DOM.clearTransactions()
+        App.init()
+    },
+}
+
+App.init()
+
+Transaction.add({
+    description: 'Comida',
+    amount: -25000,
+    date:'23/01/2021'
 })
 
-//parei com uma hora de video da aula 3
+//parei o video na parte de captura de dados time: 1:58'
